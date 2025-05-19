131d82473fce4a9331134a185af7c5eb5884be28
Nafizio-Za <nafiziozafranz@gmail.com>
2025-05-19 22:42:09 +0700

    hmz

---
+++ admin/gamehp_proses.php
@@ -0,0 +1,72 @@
+<?php
+    include "../config/connection.php";
+    if(isset($_REQUEST['status']))
+    {
+        $status = $_REQUEST['status'];
+    }
+    
+    switch ($status)
+    {
+        case 'tambah':
+            $id_gamehp=$_REQUEST['id_gamehp'];
+            $gamehp=$_REQUEST['gamehp'];
+            
+            $asal = $_FILES['gambar']['tmp_name'];
+            $simpan_gambar = "../gambar/".$_FILES['gambar']['name']; 
+            move_uploaded_file($asal, $simpan_gambar);
+            
+            $gamehp_input = mysqli_query($connection, "INSERT INTO 
+            tb_gamehp VALUES ('$id_gamehp', '$gamehp', '$simpan_gambar')");
+            if ($gamehp_input == true)
+            {
+                echo"<script>alert ('input in db')</script>";
+            }
+            else
+            {
+                echo"<script>alert ('input in db failed')</script>";
+            }
+            
+        break;
+
+case 'edit':
+    $id_gamehp = $_REQUEST['id_gamehp'];
+    $gamehp = $_REQUEST['gamehp'];
+
+    
+    if (isset($_FILES['gambar']) && $_FILES['gambar']['error'] == 0) {
+        $asal = $_FILES['gambar']['tmp_name'];
+        $simpan_gambar = "../gambar/" . $_FILES['gambar']['name'];
+        move_uploaded_file($asal, $simpan_gambar);
+        
+        $gamehp_edit = mysqli_query($connection, "UPDATE tb_gamehp SET id_gamehp = '$id_gamehp', gamehp = '$gamehp', deskripsi_gamehp = '$deskripsi_gamehp', gambar = '$simpan_gambar' WHERE id_gamehp = '$id_gamehp'");
+    } else {
+        
+        $gamehp_edit = mysqli_query($connection, "UPDATE tb_gamehp SET id_gamehp = '$id_gamehp', gamehp = '$gamehp' WHERE id_gamehp = '$id_gamehp'");
+    }
+
+    if ($gamehp_edit == true) {
+        echo "<script>alert ('update in db')</script>";
+    } else {
+        echo "<script>alert ('input in db failed')</script>";
+    }
+
+break;
+
+        case 'hapus':
+            $id_gamehp = $_REQUEST['id_gamehp'];
+
+            $gamehp_hapus = mysqli_query ($connection, "DELETE FROM tb_gamehp
+            WHERE id_gamehp = '$id_gamehp'");
+
+            if($gamehp_hapus == true)
+            {
+                echo"<script>alert ('DELETE DATA BARANG ISOK')</script>";
+            }
+            else
+            {
+                echo"<script>alert ('DELETE DATA BARANG GAISOK')</script>";
+            }
+        break;
+    }
+?>
+<meta http-equiv="refresh" content="0;URL=admin.php?page=gamehp_tampil">
--- admin/makanan_proses.php
+++ admin/makanan_proses.php
@@ -30,27 +30,39 @@
         break;
 
         case 'edit':
-            $id_makanan=$_REQUEST['id_makanan'];
-            $makanan=$_REQUEST['makanan'];
-            $deskripsi_makanan=$_REQUEST['deskripsi_makanan'];
-            
-            if ($centang == '1')
-            {
-                $asal = $_FILES['gambar']['tmp_name'];
-                $simpan_gambar = "../gambar/".$_FILES['gambar']['name']; 
+            $id_makanan = $_REQUEST['id_makanan'];
+            $makanan = $_REQUEST['makanan'];
+            $deskripsi_makanan = $_REQUEST['deskripsi_makanan'];
+            $centang = isset($_REQUEST['centang']) ? $_REQUEST['centang'] : '0';
+            $simpan_gambar = '';
+    
+            // Jika checkbox dicentang, proses upload gambar baru
+            if ($centang == '1' && isset($_FILES['gambar_makanan'])) {
+            $asal = $_FILES['gambar']['tmp_name'];
+                $nama_file = $_FILES['gambar']['name'];
+                $simpan_gambar = "../gambar/".$nama_file;
                 move_uploaded_file($asal, $simpan_gambar);
-            }
- 
-            $id_makanan = mysqli_query($connection, "UPDATE tb_makanan SET id_makanan = '$id_makanan', makanan = '$makanan', deskripsi_makanan, '$deskripsi_makanan', gambar = '$simpan_gambar' WHERE id_makanan = '$id_makanan'");
-            if ($makanan_edit == true)
-            {
-                echo"<script>alert ('update in db')</script>";
-            }
-            else
-            {
-                echo"<script>alert ('input in db failed')</script>";
+            } else {
+                // Ambil path gambar lama dari database jika tidak upload baru
+                $result = mysqli_query($connection, "SELECT gambar_makanan FROM tb_makanan WHERE id_makanan = '$id_makanan'");
+                $row = mysqli_fetch_assoc($result);
+                $simpan_gambar = $row['gambar'];
             }
             
+            // Query UPDATE yang benar
+            $query = "UPDATE tb_makanan SET 
+                      makanan = '$makanan', 
+                      deskripsi_makanan = '$deskripsi_makanan', 
+                      gambar_makanan = '$simpan_gambar' 
+                      WHERE id_makanan = '$id_makanan'";
+            
+            $makanan_edit = mysqli_query($connection, $query);
+            
+            if ($makanan_edit) {
+                echo "<script>alert('Update berhasil')</script>";
+            } else {
+                echo "<script>alert('Gagal update: ".mysqli_error($connection)."')</script>";
+            }
         break;
 
         case 'hapus':
